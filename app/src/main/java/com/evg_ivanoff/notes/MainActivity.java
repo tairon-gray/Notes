e09c7f9b47b4e377af91613605f8d71406163257
Eugene Ivanov <wied11@yandex.ru>
2022-02-16 00:20:55 +0500

    Реализация механизма добавления новой заметки Добавление методов на нажатие, долгое нажатие, свайп влево\вправо

--- app/src/main/java/com/evg_ivanoff/notes/MainActivity.java
+++ app/src/main/java/com/evg_ivanoff/notes/MainActivity.java
@@ -1,12 +1,16 @@
 package com.evg_ivanoff.notes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.ItemTouchUIUtil;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -14,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerViewNotes;
     public static final ArrayList<Note> notes = new ArrayList<>();
+    private NoteAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +39,38 @@ public class MainActivity extends AppCompatActivity {
             notes.add(new Note("Строитель", "Построить дом", "воскресенье", 2));
             notes.add(new Note("Учитель", "Выучить уроки", "вторник", 2));
         }
-        NoteAdapter adapter = new NoteAdapter(notes);
+        adapter = new NoteAdapter(notes);
         recyclerViewNotes.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
         recyclerViewNotes.setAdapter(adapter);
+        adapter.setOnNoteClickListener(new NoteAdapter.OnNoteClickListener() {
+            @Override
+            public void onNoteClick(int position) {
+                Toast.makeText(MainActivity.this, "clicked", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onLongClick(int position) {
+                removeNote(position);
+            }
+        });
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                removeNote(viewHolder.getAdapterPosition());
+            }
+        });
+        itemTouchHelper.attachToRecyclerView(recyclerViewNotes);
+
+    }
+
+    private void removeNote(int position){
+        notes.remove(position);
+        adapter.notifyDataSetChanged();
     }
 
     public void onClickNewNote(View view) {
