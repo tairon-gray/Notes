f0119926122a615cb860f3048a8061725b788a7b
Eugene Ivanov <wied11@yandex.ru>
2022-02-15 22:32:30 +0500

    Реализована работа recyclerView, добавлен класс Заметки и адаптер для её отображения

---
+++ app/src/main/java/com/evg_ivanoff/notes/NoteAdapter.java
@@ -0,0 +1,51 @@
+package com.evg_ivanoff.notes;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NotesViewHolder>{
+    private ArrayList<Note> notes;
+
+    public NoteAdapter(ArrayList<Note> notes) {
+        this.notes = notes;
+    }
+
+    @NonNull
+    @Override
+    public NotesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_view, parent, false);
+        return new NotesViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NotesViewHolder holder, int position) {
+        Note note = notes.get(position);
+        holder.textViewTitle.setText(note.getTitle());
+        holder.textViewDescription.setText(note.getDescription());
+        holder.textViewDayOfWeek.setText(note.getDayOfWeek());
+        holder.textViewPriority.setText(String.format("%s", note.getPriority()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    class NotesViewHolder extends RecyclerView.ViewHolder {
+        private TextView textViewTitle, textViewDescription, textViewDayOfWeek, textViewPriority;
+        public NotesViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewTitle = itemView.findViewById(R.id.textViewTitle);
+            textViewDescription = itemView.findViewById(R.id.textViewDescription);
+            textViewDayOfWeek = itemView.findViewById(R.id.textViewDayOfWeek);
+            textViewPriority = itemView.findViewById(R.id.textViewPriority);
+        }
+    }
+}
