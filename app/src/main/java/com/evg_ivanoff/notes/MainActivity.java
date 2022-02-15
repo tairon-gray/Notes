6e30b94b1e99a3ea4e99813faf32420db1fdbcf8
Eugene Ivanov <wied11@yandex.ru>
2022-02-16 00:00:45 +0500

    Реализация механизма добавления новой заметки

--- app/src/main/java/com/evg_ivanoff/notes/MainActivity.java
+++ app/src/main/java/com/evg_ivanoff/notes/MainActivity.java
@@ -4,14 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerViewNotes;
-    private ArrayList<Note> notes = new ArrayList<>();
+    public static final ArrayList<Note> notes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,20 +21,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         recyclerViewNotes = findViewById(R.id.recyclerViewNotes);
-        notes.add(new Note("Стоматолог", "Вылечить зубы", "понедельник", 2));
-        notes.add(new Note("Парикмахер", "Сделать прическу", "среда", 3));
-        notes.add(new Note("Строитель", "Построить дом", "воскресенье", 2));
-        notes.add(new Note("Учитель", "Выучить уроки", "вторник", 2));
-        notes.add(new Note("Тренер", "Потренироваться", "понедельник", 1));
-        notes.add(new Note("Визажист", "Сделать макияж", "суббота", 3));
-        notes.add(new Note("Мотиватор", "Получить мотивацию", "пятница", 3));
-        notes.add(new Note("Стоматолог", "Вылечить зубы", "понедельник", 2));
-        notes.add(new Note("Парикмахер", "Сделать прическу", "среда", 3));
-        notes.add(new Note("Строитель", "Построить дом", "воскресенье", 2));
-        notes.add(new Note("Учитель", "Выучить уроки", "вторник", 2));
-
+        if (notes.isEmpty()) {
+            notes.add(new Note("Стоматолог", "Вылечить зубы", "понедельник", 2));
+            notes.add(new Note("Парикмахер", "Сделать прическу", "среда", 3));
+            notes.add(new Note("Строитель", "Построить дом", "воскресенье", 2));
+            notes.add(new Note("Учитель", "Выучить уроки", "вторник", 2));
+            notes.add(new Note("Тренер", "Потренироваться", "понедельник", 1));
+            notes.add(new Note("Визажист", "Сделать макияж", "суббота", 3));
+            notes.add(new Note("Мотиватор", "Получить мотивацию", "пятница", 3));
+            notes.add(new Note("Стоматолог", "Вылечить зубы", "понедельник", 2));
+            notes.add(new Note("Парикмахер", "Сделать прическу", "среда", 3));
+            notes.add(new Note("Строитель", "Построить дом", "воскресенье", 2));
+            notes.add(new Note("Учитель", "Выучить уроки", "вторник", 2));
+        }
         NoteAdapter adapter = new NoteAdapter(notes);
         recyclerViewNotes.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
         recyclerViewNotes.setAdapter(adapter);
+    }
+
+    public void onClickNewNote(View view) {
+        Intent intent = new Intent(this, AddNote.class);
+        startActivity(intent);
     }
 }
